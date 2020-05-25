a3a7d61c74a55e5618cc78ef6ce3a273127b9391
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-05-25 21:05:26 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +14,6 @@
 */
 
 Route::get('/', 'FoundController@index')->name('found');
-
 
 Auth::routes();
 
