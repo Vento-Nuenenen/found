f2cdeeb2717c8bb8e2c9f224f97c6c49d6cadb40
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-06-18 21:22:36 +0200

    More models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,4 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model{
     protected $table = 'events';
+
+    protected $fillable = ['event_name', 'event_date', 'event_active'];
 }
--- app/Models/Group.php
+++ app/Models/Group.php
@@ -6,4 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model{
     protected $table = 'groups';
+
+    protected $fillable = ['group_name', 'group_active'];
 }
