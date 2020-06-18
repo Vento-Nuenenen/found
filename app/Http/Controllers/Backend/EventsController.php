6ecca84c7dd1010d1cd3104ce10ed7584532d802
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-06-18 21:22:29 +0200

    More controllers

--- app/Http/Controllers/Backend/EventsController.php
+++ app/Http/Controllers/Backend/EventsController.php
@@ -3,11 +3,103 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
+use App\Models\Group;
+use DB;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class EventsController extends Controller
 {
-    public function index(){
-        return view('backend.events.events');
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function index(Request $request){
+        if ($request->input('search') == null) {
+            $events = Event::all();
+        } else {
+            $search_string = $request->input('search');
+            $events = DB::table('events')
+                ->select('events.*')
+                ->where('events.event_name', 'LIKE', "%$search_string%")->get();
+        }
+
+        return view('backend.events.events', ['events' => $events]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function create(){
+        return view('backend.events.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     *
+     * @return RedirectResponse
+     */
+    public function store(Request $request){
+        $group_name = $request->input('group_name');
+        $group_active = $request->input('group_active');
+
+        Group::create([
+            'group_name' => $group_name,
+            'group_active' => $group_active,
+        ]);
+
+        return redirect()->back()->with('message', 'Event wurde erstellt.');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param $eid
+     *
+     * @return Application|Factory|View
+     */
+    public function edit($eid){
+        $groups = Group::where('id', '=', $eid)->first();
+
+        return view('backend.groups.edit', ['groups' => $groups]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param $eid
+     *
+     * @return RedirectResponse
+     */
+    public function update(Request $request, $eid){
+        $event_name = $request->input('event_name');
+        $event_date = $request->input('event_date');
+
+        DB::table('events')->where('id', '=', $eid)->update(['event_name' => $event_name, 'event_date' => $event_date]);
+
+        return redirect()->back()->with('message', 'Event wurde aktualisiert.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param $eid
+     *
+     * @return RedirectResponse
+     */
+    public function destroy($eid){
+        DB::table('events')->where('id', '=', $eid)->delete();
+
+        return redirect()->back()->with('message', 'Event erfolgreich gelöscht.');
     }
 }
--- app/Http/Controllers/Backend/GroupsController.php
+++ app/Http/Controllers/Backend/GroupsController.php
@@ -5,14 +5,19 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Group;
 use DB;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class GroupsController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View
      */
     public function index(Request $request)
     {
@@ -27,10 +32,11 @@ class GroupsController extends Controller
 
         return view('backend.groups.groups', ['groups' => $groups]);
     }
+
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View
      */
     public function create(){
         return view('backend.groups.add');
@@ -39,17 +45,19 @@ class GroupsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      *
-     * @return void
+     * @param $group_active
+     *
+     * @return RedirectResponse
      */
     public function store(Request $request){
         $group_name = $request->input('group_name');
-        $group_active = $request->
+        $group_active = $request->input('group_active') == "on";
 
         Group::create([
             'group_name' => $group_name,
-            'group_active' =>
+            'group_active' => $group_active,
         ]);
 
         return redirect()->back()->with('message', 'Gruppe wurde erstellt.');
@@ -60,41 +68,37 @@ class GroupsController extends Controller
      *
      * @param $gid
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View
      */
     public function edit($gid){
-        $groups = DB::table('groups')->where('id', '=', $gid)->first();
+        $groups = Group::where('id', '=', $gid)->first();
 
         return view('backend.groups.edit', ['groups' => $groups]);
     }
+
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @param $gid
      *
-     * @return \Illuminate\Http\Response
+     * @return RedirectResponse
      */
     public function update(Request $request, $gid){
         $group_name = $request->input('group_name');
+        $group_active = $request->input('group_active') == 'on';
 
-        if($request->file('tn_img')){
-            $logo_name = time() .'.' . $request->file('group_logo')->extension();
-            $request->file('group_logo')->move(storage_path('app/public/img'), $logo_name);
-        }else{
-            $logo_name = null;
-        }
-
-        DB::table('groups')->where('id', '=', $gid)->update(['group_name' => $group_name, 'logo_file_name' => $logo_name]);
+        DB::table('groups')->where('id', '=', $gid)->update(['group_name' => $group_name, 'group_active' => $group_active]);
 
         return redirect()->back()->with('message', 'Gruppe wurde aktualisiert.');
     }
+
     /**
      * Remove the specified resource from storage.
      *
      * @param $gid
      *
-     * @return \Illuminate\Http\Response
+     * @return RedirectResponse
      */
     public function destroy($gid){
         DB::table('groups')->where('id', '=', $gid)->delete();
