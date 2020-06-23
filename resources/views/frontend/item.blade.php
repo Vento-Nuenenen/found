2bc2ce4e9bcf75d9892b0231b8501bc5f984e4a2
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-06-23 20:44:46 +0200

    Refactor nav

--- resources/views/frontend/item.blade.php
+++ resources/views/frontend/item.blade.php
@@ -14,74 +14,70 @@
 
                 <a href="{{  route('frontend') }}" class="float-right">Zurück zur Übersicht</a>
             </div>
-            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent=".User">
-                <div class="card-body">
-                    <div class="float-left">
-                        <p>
+            <div class="card-body">
+                <div class="float-left">
+                    <p>
                         <h5>Farbe: {{ $item->item_color ?? 'Keine Angabe' }}</h5>
-                        </p>
-                        <p>
+                    </p>
+                    <p>
                         <h5>Grösse: {{ $item->item_size ?? 'Keine Angabe' }}</h5>
-                        </p>
-                        <p>
+                    </p>
+                    <p>
                         <h5>Event: {{ $item->event->event_name ?? 'Keine Angabe' }}</h5>
-                        </p>
-                        <p>
+                    </p>
+                    <p>
                         <h5>Gruppe: {{ $item->group->group_name ?? 'Keine Angabe' }}</h5>
-                        </p>
-                    </div>
-                    <div>
-                        <img class="col-6 rounded float-right" src="{{ $item->item_img ? asset('storage/img/' . $item->img_name) : asset("/storage/placeholder-".random_int(1, 3).".png") }}" alt="Item image">
-                    </div>
+                    </p>
+                </div>
+                <div>
+                    <img class="col-6 rounded float-right" src="{{ $item->item_img ? asset('storage/img/' . $item->img_name) : asset("/storage/placeholder-".random_int(1, 3).".png") }}" alt="Item image">
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="clearfix p-3"></div>
+    <div class="clearfix p-3"></div>
 
-        <div class="card">
-            <div class="card-header">
-                <h5 class="float-left">Das ist meines!</h5>
+    <div class="card">
+        <div class="card-header">
+            <h5>Das ist meines!</h5>
+        </div>
+        <div class="card-body">
+            <div class="form-group has-feedback row {{ $errors->has('customer_name') ? ' has-error ' : '' }}">
+                {!! Form::label('customer_name', 'Vor & Nachname', array('class' => 'col-md-3 control-label')); !!}
+                <div class="col-md-9">
+                    <div class="input-group">
+                        {!! Form::text('customer_name', NULL, array('id' => 'item_identifier', 'class' => 'form-control', 'placeholder' => 'Vor & Nachname', 'required')) !!}
+                        <div class="input-group-append">
+                            <label class="input-group-text" for="item_identifier">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                            </label>
+                        </div>
+                    </div>
+                    @if ($errors->has('customer_name'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('customer_name') }}</strong>
+                        </span>
+                    @endif
+                </div>
             </div>
-            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent=".User">
-                <div class="card-body">
-                    <div class="form-group has-feedback row {{ $errors->has('customer_name') ? ' has-error ' : '' }}">
-                        {!! Form::label('customer_name', 'Vor & Nachname', array('class' => 'col-md-3 control-label')); !!}
-                        <div class="col-md-9">
-                            <div class="input-group">
-                                {!! Form::text('customer_name', NULL, array('id' => 'item_identifier', 'class' => 'form-control', 'placeholder' => 'Vor & Nachname', 'required')) !!}
-                                <div class="input-group-append">
-                                    <label class="input-group-text" for="item_identifier">
-                                        <i class="fa fa-user" aria-hidden="true"></i>
-                                    </label>
-                                </div>
-                            </div>
-                            @if ($errors->has('customer_name'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('customer_name') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                    </div>
 
-                    <div class="form-group has-feedback row {{ $errors->has('customer_mail') ? ' has-error ' : '' }}">
-                        {!! Form::label('customer_mail', 'E-Mail', array('class' => 'col-md-3 control-label')); !!}
-                        <div class="col-md-9">
-                            <div class="input-group">
-                                {!! Form::email('customer_mail', NULL, array('id' => 'customer_mail', 'class' => 'form-control', 'placeholder' => 'E-Mail', 'required')) !!}
-                                <div class="input-group-append">
-                                    <label class="input-group-text" for="customer_mail">
-                                        <i class="fa fa-at" aria-hidden="true"></i>
-                                    </label>
-                                </div>
-                            </div>
-                            @if ($errors->has('customer_mail'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('customer_mail') }}</strong>
-                                </span>
-                            @endif
+            <div class="form-group has-feedback row {{ $errors->has('customer_mail') ? ' has-error ' : '' }}">
+                {!! Form::label('customer_mail', 'E-Mail', array('class' => 'col-md-3 control-label')); !!}
+                <div class="col-md-9">
+                    <div class="input-group">
+                        {!! Form::email('customer_mail', NULL, array('id' => 'customer_mail', 'class' => 'form-control', 'placeholder' => 'E-Mail', 'required')) !!}
+                        <div class="input-group-append">
+                            <label class="input-group-text" for="customer_mail">
+                                <i class="fa fa-at" aria-hidden="true"></i>
+                            </label>
                         </div>
                     </div>
+                    @if ($errors->has('customer_mail'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('customer_mail') }}</strong>
+                        </span>
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -15,54 +15,16 @@
                         Neues
                     </a>
                 </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Artikel nach Gruppen
-                    </a>
-
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item {{ Request::is('users/')}}" href="{{ url('/users') }}">
-                            Benutzer
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item {{ Request::is('groups/')}}" href="{{ url('/groups') }}">
-                            Item Gruppen
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item {{ Request::is('participations/')}}" href="{{ url('/participations') }}">
-                            Teilnehmer
-                        </a>
-                        <a class="dropdown-item {{ Request::is('passed/')}}" href="{{ url('/passed') }}">
-                            Bestanden
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item {{ Request::is('numbers/')}}" href="{{ url('/numbers') }}">
-                            Notfallnummern
-                        </a>
-                    </div>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Artikel nach Anlass
-                    </a>
-
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item {{ Request::is('points/')}}" href="{{ url('/points') }}">
-                            Punkte übersicht
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item {{ Request::is('transactions/')}}" href="{{ url('/transactions') }}">
-                            Punkte transaktionen
-                        </a>
-                    </div>
-                </li>
             </ul>
 
-            @if(!Auth::guest())
-            <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                @else
                     <li class="nav-item">
                         <a class="nav-link {{ Request::is('admin/overwatch/')}}" href="{{ url('/admin/overwatch') }}">
                             Übersicht
@@ -96,17 +58,7 @@
                             </a>
                         </div>
                     </li>
-                </ul>
-            @endif
 
-        <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                @else
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->scout_name }} <span class="caret"></span>
