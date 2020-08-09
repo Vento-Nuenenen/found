2a23b52f6ffe248aa236c5e6c1158a26e84d5506
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-08-09 20:03:13 +0200

    Update controller

--- app/Http/Controllers/Backend/ClaimedController.php
+++ app/Http/Controllers/Backend/ClaimedController.php
@@ -21,9 +21,29 @@ class ClaimedController extends Controller
         return view('backend.claimed.show', ['claim' => $claim]);
     }
 
+    public function update($cid, Request $request){
+        $action = $request->action;
+
+        if($action == 'returned'){
+            $claim = Customer::with('Item')->find($cid)->first();
+
+            $claim->item->item_returned = true;
+            $claim->push();
+
+            print_r($claim);
+        }else if($action == 'sold'){
+            $claim = Customer::with('Item')->find($cid)->first();
+
+            $claim->item->item_sold = true;
+            $claim->push();
+        }
+
+        return redirect()->back()->with('message', 'Änderung wurde vorgenommen');
+    }
+
     public function destroy($cid){
         Customer::destroy($cid);
 
-        return redirect()->back()->with('message', 'Customer erfolgreich gelöscht.');
+        return redirect()->back()->with('message', 'Claim erfolgreich gelöscht.');
     }
 }
