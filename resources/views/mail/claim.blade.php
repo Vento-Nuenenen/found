b48c9a616dbefc19a309f45626f7f5898a0c044e
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2020-08-12 19:43:41 +0200

    Implement mail styling

--- resources/views/mail/claim.blade.php
+++ resources/views/mail/claim.blade.php
@@ -1,27 +1,314 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+    <head>
+        <title>Lost & Found - Pfadi Nünenen</title>
 
-@section('content')
-    <div class="container">
-        @if(session()->has('message'))
-            <div class="alert alert-success">
-                {{ session()->get('message') }}
-            </div>
-        @endif
+        <!--[if !mso]><!-- -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!--<![endif]-->
 
-        <div class="card User mb-3">
-            <div class="card-header" id="headingOne">
-                <h5 class="mb-0">
-                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
-                            aria-controls="collapseOne">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                    </button>
-                </h5>
-            </div>
-            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent=".User">
-                <div class="card-body table-responsive">
+        <style type="text/css">
+            #outlook a { padding:0; }
+            .ReadMsgBody { width:100%; }
+            .ExternalClass { width:100%; }
+            .ExternalClass * { line-height:100%; }
+            body { margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; }
+            table, td { border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt; }
+            img { border:0;height:auto;line-height:100%; outline:none;text-decoration:none;-ms-interpolation-mode:bicubic; }
+            p { display:block;margin:13px 0; }
+        </style>
 
-                </div>
-            </div>
-        </div>
+        <!--[if !mso]><!-->
+        <style type="text/css">
+            @media only screen and (max-width:480px) {
+                @-ms-viewport { width:320px; }
+                @viewport { width:320px; }
+            }
+        </style>
+        <!--<![endif]-->
+
+        <!--[if mso]>
+        <xml>
+            <o:OfficeDocumentSettings>
+                <o:AllowPNG/>
+                <o:PixelsPerInch>96</o:PixelsPerInch>
+            </o:OfficeDocumentSettings>
+        </xml>
+        <![endif]-->
+
+        <!--[if lte mso 11]>
+        <style type="text/css">
+            .outlook-group-fix { width:100% !important; }
+        </style>
+        <![endif]-->
+
+        <!--[if !mso]><!-->
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Cabin:400,700" rel="stylesheet" type="text/css">
+        <style type="text/css">
+            @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+            @import url(https://fonts.googleapis.com/css?family=Cabin:400,700);
+        </style>
+        <!--<![endif]-->
+
+        <style type="text/css">
+            @media only screen and (min-width:480px) {
+                .mj-column-per-100 { width:100% !important; max-width: 100%; }
+                .mj-column-per-60 { width:60% !important; max-width: 60%; }
+                .mj-column-per-40 { width:40% !important; max-width: 40%; }
+            }
+        </style>
+
+        <style type="text/css">
+            @media only screen and (max-width:480px) {
+                table.full-width-mobile { width: 100% !important; }
+                td.full-width-mobile { width: auto !important; }
+            }
+        </style>
+
+        <style type="text/css">.hide_on_mobile { display: none !important;}
+            @media only screen and (min-width: 480px) { .hide_on_mobile { display: block !important;} }
+            .hide_section_on_mobile { display: none !important;}
+            @media only screen and (min-width: 480px) { .hide_section_on_mobile { display: table !important;} }
+            .hide_on_desktop { display: block !important;}
+            @media only screen and (min-width: 480px) { .hide_on_desktop { display: none !important;} }
+            .hide_section_on_desktop { display: table !important;}
+            @media only screen and (min-width: 480px) { .hide_section_on_desktop { display: none !important;} }
+
+            [owa] .mj-column-per-100 {
+                width: 100%!important;
+            }
+
+            [owa] .mj-column-per-50 {
+                width: 50%!important;
+            }
+
+            [owa] .mj-column-per-33 {
+                width: 33.333333333333336%!important;
+            }
+
+            p {
+                margin: 0px;
+            }
+
+            @media only print and (min-width:480px) {
+                .mj-column-per-100 { width:100%!important; }
+                .mj-column-per-40 { width:40%!important; }
+                .mj-column-per-60 { width:60%!important; }
+                .mj-column-per-50 { width: 50%!important; }
+                mj-column-per-33 { width: 33.333333333333336%!important; }
+            }
+        </style>
+    </head>
+    <body style="background-color:#f6f7ec;">
+        <div style="background-color:#f6f7ec;">
+
+    <!--[if mso | IE]>
+    <table
+        align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600">
+        <tr>
+            <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+    <![endif]-->
+
+    <div style="background:#212e49;background-color:#212e49;Margin:0px auto;max-width:600px;">
+
+        <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#212e49;background-color:#212e49;width:100%;">
+            <tbody>
+            <tr>
+                <td style="direction:ltr;font-size:0px;padding:9px 0px 9px 0px;text-align:center;vertical-align:top;">
+                    <!--[if mso | IE]>
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+
+                        <tr>
+
+                            <td
+                                class="" style="vertical-align:top;width:600px;"
+                            >
+                    <![endif]-->
+
+                    <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+
+                            <tbody><tr>
+                                <td align="center" style="font-size:0px;padding:0px 0px 0px 0px;word-break:break-word;">
+
+                                    <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
+                                        <tbody>
+                                        <tr>
+                                            <td style="width:600px;">
+
+                                                <a href="https://pfadi-nuenenen.ch/" target="_blank" style="color: #FF00EC;">
+
+                                                    <img alt="Pfadi N&#xFC;nenen" height="auto" src="./images/1597249049.jpg" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="600">
+
+                                                </a>
+
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td align="center" style="font-size:0px;padding:10px 15px 10px 15px;word-break:break-word;">
+
+                                    <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:11px;line-height:1.2;text-align:center;color:#000000;">
+                                        <h1 style="font-family: &apos;Cabin&apos;, sans-serif; text-align: center; line-height: 100%;"><span style="font-size: 24px; color: #ecf0f1;">Lost &amp; Found - Pfadi N&#xFC;nenen</span></h1>
+                                    </div>
+
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td align="left" style="font-size:0px;padding:15px 15px 15px 15px;word-break:break-word;">
+
+                                    <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:11px;line-height:1.8;text-align:left;color:#F8F7F7;">
+                                        <p>Hallo $name</p>
+                                        <p>&#xA0;</p>
+                                        <p>Danke das du dich bei uns gemeldet hat, um einen Fundgegenstand abzuholen.</p>
+                                        <p>Wir werden uns demn&#xE4;chst bei dir melden, um die Details auszuhandeln.</p>
+                                        <p>Nachfolgend siehst du noch einmal, was du als Verloren gemeldet hast.</p>
+                                        <p>&#xA0;</p>
+                                        <p>Mit den besten Gr&#xFC;ssen</p>
+                                        <p>Die N&#xFC;nenen-Leitenden</p>
+                                    </div>
+
+                                </td>
+                            </tr>
+
+                            </tbody></table>
+
+                    </div>
+
+                    <!--[if mso | IE]>
+                    </td>
+
+                    </tr>
+
+                    </table>
+                    <![endif]-->
+                </td>
+            </tr>
+            </tbody>
+        </table>
+
     </div>
-@endsection
+
+
+    <!--[if mso | IE]>
+    </td>
+    </tr>
+    </table>
+
+    <table
+        align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600"
+    >
+        <tr>
+            <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+    <![endif]-->
+
+
+    <div style="background:#212e49;background-color:#212e49;Margin:0px auto;max-width:600px;">
+
+        <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#212e49;background-color:#212e49;width:100%;">
+            <tbody>
+            <tr>
+                <td style="direction:ltr;font-size:0px;padding:9px 0px 9px 0px;text-align:center;vertical-align:top;">
+                    <!--[if mso | IE]>
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+
+                        <tr>
+
+                            <td
+                                class="" style="vertical-align:top;width:360px;"
+                            >
+                    <![endif]-->
+
+                    <div class="mj-column-per-60 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+
+                            <tbody><tr>
+                                <td align="left" style="font-size:0px;padding:15px 15px 15px 15px;word-break:break-word;">
+
+                                    <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:11px;line-height:1.8;text-align:left;color:#FFFFFF;">
+                                        <ul>
+                                            <li>ID: 1</li>
+                                            <li>Oberteil</li>
+                                            <li>Gr&#xF6;sse</li>
+                                            <li>Farbe</li>
+                                            <li>&#xA0;</li>
+                                        </ul>
+                                    </div>
+
+                                </td>
+                            </tr>
+
+                            </tbody></table>
+
+                    </div>
+
+                    <!--[if mso | IE]>
+                    </td>
+
+                    <td
+                        class="" style="vertical-align:top;width:240px;"
+                    >
+                    <![endif]-->
+
+                    <div class="mj-column-per-40 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+
+                            <tbody><tr>
+                                <td align="center" style="font-size:0px;padding:0px 0px 0px 0px;word-break:break-word;">
+
+                                    <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
+                                        <tbody>
+                                        <tr>
+                                            <td style="width:240px;">
+
+                                                <img height="auto" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="240">
+
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+
+                                </td>
+                            </tr>
+
+                            </tbody></table>
+
+                    </div>
+
+                    <!--[if mso | IE]>
+                    </td>
+
+                    </tr>
+
+                    </table>
+                    <![endif]-->
+                </td>
+            </tr>
+            </tbody>
+        </table>
+
+    </div>
+
+
+        <!--[if mso | IE]>
+                </td>
+            </tr>
+        </table>
+        <![endif]-->
+
+        </div>
+    </body>
+</html>
