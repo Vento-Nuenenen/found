abb7b29f842e1b7a5487341bb09bb3b2c88f9215
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-04-30 16:52:01 +0200

    Rename seed folder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
@@ -16,7 +19,13 @@ class UsersSeeder extends Seeder
         $seededAdminEmail = 'admin@found.ch';
         $user = User::where('email', '=', $seededAdminEmail)->first();
         if ($user === null) {
-            $user = User::create(['scout_name' => 'Admin', 'first_name' => 'Admin', 'last_name' => 'Admin', 'email' => $seededAdminEmail, 'password' => Hash::make('password'),]);
+            $user = User::create([
+                'scout_name' => 'Admin',
+                'first_name' => 'Admin',
+                'last_name' => 'Admin',
+                'email' => $seededAdminEmail,
+                'password' => Hash::make('password'),
+            ]);
             $user->save();
         }
 
